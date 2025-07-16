8b89ea865fc7f991111c23bb04c1dc8c32307ff0
robel <zekaryasgebeyaw48@gmail.com>
2025-07-16 07:03:09 -0700

    7 makekigrations and migrate

--- courses/models.py
+++ courses/models.py
@@ -4,23 +4,24 @@ from django.db import models
 class AcessRequirement(models.TextChoices):
     ANYONE="anyone","Anyone"
     EMAIL_REQUIRED="email_required","Email_Required"
-    DRAFT="draft","DRAFT"
+   
 
 class PublishedStatus(models.TextChoices):
     PUBLISHED="published","PUBLISHED"
     COMING_SOON="soon","Coming Soon"
     DRAFT="draft","DRAFT"
-
-class courses(models.Model):
+def handle_upload(instance,filename):
+    return f"{filename}"
+class Courses(models.Model):
     title=models.CharField(max_length=100)
     description=models.TextField(blank=True,default="")
-    #image
-    access=models.CharField( max_length=20,
+    image=models.ImageField(upload_to=handle_upload)
+    access=models.CharField( max_length=100,
                                    choices=AcessRequirement.choices,
-                                   defualt=AcessRequirement.DRAFT)
+                                   default=AcessRequirement.EMAIL_REQUIRED)
     status=models.CharField( max_length=20,
-                                   choices=PublishedStatus.PUBLISHED,
-                                   defualt=PublishedStatus.DRAFT)
+                                   choices=PublishedStatus.choices,
+                                   default=PublishedStatus.DRAFT)
 @property
 def is_published(self):
     return self.status == PublishedStatus.PUBLISHED
